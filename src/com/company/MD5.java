897e235407d140de1d703435cee952e31593b8e5
kakassicr <yechanghust@sina.com>
2016-10-15 18:14:02 +0800

    fix

--- src/com/company/MD5.java
+++ src/com/company/MD5.java
@@ -34,6 +34,6 @@ public class MD5 {
     }
 
     public static void main(String[] args) {
-        System.out.println( getMD5("Hello".getBytes()));
+        System.out.println( getMD5("Hello kaka".getBytes()));
     }
 }
